61e58076b33f32ec51388565c5b32e7bf50536f0
Paweł Konior <pawel.konior@gmail.com>
2022-04-07 21:43:39 +0200

    all necessary styles for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import {useEffect, useState} from "react";
 
-import {collection,  doc, writeBatch, onSnapshot} from "firebase/firestore";
+import {collection, doc, writeBatch, onSnapshot} from "firebase/firestore";
 
 import Headline from "./components/Headline";
 import TaskInput from "./components/TaskInput";
@@ -41,28 +41,39 @@ function App() {
         <div className="App">
             <Headline/>
             <div className="container">
-            <TaskInput/>
-            {tasks.length === 0 ? ('') : (
-                <>
-                    <TaskList
-                        tasks={tasks}
-                        selection={selection}
-                    />
-                    {/*TODO move to separate component */}
-                    <p>{tasks.filter((e) => !e.status).length} items left</p>
+                <TaskInput/>
+                {tasks.length === 0 ? ('') : (
+                    <>
+                        <TaskList
+                            tasks={tasks}
+                            selection={selection}
+                        />
 
-                    {/*TODO move to separate component */}
-                    <div>
-                        <button onClick={() => setSelection('all')}>All</button>
-                        <button onClick={() => setSelection(false)}>Active</button>
-                        <button onClick={() => setSelection(true)}>Completed</button>
-                    </div>
+                        <div className="utils">
+                            {/*TODO move to separate component */}
+                            <p>{tasks.filter((e) => !e.status).length} items left</p>
 
-                    {/*TODO move to separate component */}
-                    {tasks.filter((e) => e.status).length > 0 ? (
-                        <button onClick={handleDeleteDone}>Clear Completed</button>) : ('')}
-                </>)
-            }
+                            {/*TODO move to separate component */}
+                            <div>
+                                <button onClick={() => setSelection('all')}
+                                        className={selection === 'all' ? 'active' : ''}>All
+                                </button>
+                                <button onClick={() => setSelection(false)}
+                                        className={selection === false ? 'active' : ''}>Active
+                                </button>
+                                <button onClick={() => setSelection(true)}
+                                        className={selection === true ? 'active' : ''}>Completed
+                                </button>
+                            </div>
+
+                            {/*TODO move to separate component */}
+                            <div>
+                                {tasks.filter((e) => e.status).length > 0 ? (
+                                    <button onClick={handleDeleteDone}>Clear Completed</button>) : ('')}
+                            </div>
+                        </div>
+                    </>)
+                }
             </div>
         </div>
     );
